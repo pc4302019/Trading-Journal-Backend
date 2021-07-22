26bdc763fefdc3b86fd2339f73eb832ee227fb49
Prateek Chourasia <prateekngu@gmail.com>
2021-07-22 18:45:46 +0530

    first commit: untracked files added

---
+++ src/main/java/com/example/Journal/MonthlyTradingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyTradingApplication.class, args);
+	}
+
+}
